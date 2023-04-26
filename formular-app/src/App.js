ab495ff686d7a39e6ea7258f68f6df021a14207b
Jakub Dvořák <jakubdvorak91@seznam.cz>
2023-04-26 19:28:18 +0200

    vypisování formuláře v formular-app

---
+++ formular-app/src/App.js
@@ -0,0 +1,78 @@
+import { useState } from "react"
+
+const App = () => {
+    const [oneUser, setOneUser] = useState({ fullName: "", email: "", age: "" })
+    const [users, setUsers] = useState([])
+
+    const formChange = (event) => {
+        const name = event.target.name
+        const value = event.target.value
+
+        setOneUser({ ...oneUser, [name]: value })
+    }
+
+    const formSubmit = (event) => {
+        event.preventDefault()
+
+        if (oneUser.fullName && oneUser.email && oneUser.age) {
+            const newUser = {
+                fullName: oneUser.fullName,
+                email: oneUser.email,
+                age: oneUser.age,
+            }
+            setUsers((users) => {
+                return [...users, newUser]
+            })
+
+            setOneUser({ fullName: "", email: "", age: "" })
+        } else {
+            console.log("něco neylo vyplněno")
+        }
+    }
+
+    return (
+        <article>
+            <form onSubmit={formSubmit}>
+                <input
+                    value={oneUser.fullName}
+                    onChange={formChange}
+                    className="userInfo"
+                    type="text"
+                    placeholder="Jméno"
+                    name="fullName"
+                />
+                <input
+                    value={oneUser.email}
+                    onChange={formChange}
+                    className="userInfo"
+                    type="email"
+                    placeholder="Email"
+                    name="email"
+                />
+                <input
+                    value={oneUser.age}
+                    onChange={formChange}
+                    className="userInfo"
+                    type="text"
+                    placeholder="Věk"
+                    name="age"
+                />
+                <input type="submit" />
+            </form>
+
+            {users.map((oneUser, index) => {
+                const { fullName, email, age } = oneUser
+
+                return (
+                    <div key={index} className="item">
+                        <h2>{fullName}</h2>
+                        <p>{email}</p>
+                        <p>{age}</p>
+                    </div>
+                )
+            })}
+        </article>
+    )
+}
+
+export default App
