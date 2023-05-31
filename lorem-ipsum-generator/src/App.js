3acfc29caf5451459fc5df3085387c243f8c47ed
Jakub Dvořák <jakubdvorak91@seznam.cz>
2023-05-31 18:17:06 +0200

    logika aplikace lorem ipsum generator

--- lorem-ipsum-generator/src/App.js
+++ lorem-ipsum-generator/src/App.js
@@ -1,5 +1,40 @@
+import data from "./data.js"
+import { useState } from "react"
+
 const App = () => {
-    return <h1>test</h1>
+    const [count, setCount] = useState(0)
+    const [paragraphs, setParagraphs] = useState([])
+
+    const submitForm = (e) => {
+        e.preventDefault()
+        const amount = parseInt(count)
+
+        setParagraphs(data.slice(0, amount))
+    }
+
+    return (
+        <section>
+            <h1> Lorem Ipsum Generator (max. 10 odstavců)</h1>
+            <form onSubmit={submitForm}>
+                <label htmlFor="paragraphs">Odstavce:</label>
+                <input
+                    type="number"
+                    id="paragraphs"
+                    onChange={(e) => {
+                        setCount(e.target.value)
+                    }}
+                />
+                <br />
+                <input type="submit" value="vygenerovat" />
+            </form>
+
+            <article>
+                {paragraphs.map((oneParagraph, index) => {
+                    return <p key={index}>{oneParagraph}</p>
+                })}
+            </article>
+        </section>
+    )
 }
 
 export default App
