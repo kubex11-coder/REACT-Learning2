d5e4c698dd228ff7a8a74e139badb820c40844eb
kubex11-coder <jakubdvorak91@seznam.cz>
2024-02-20 10:51:50 +0100

    pokračování projektu portfolio1
    pokračování projektu portfolio1

--- portfolio-web1/src/App.js
+++ portfolio-web1/src/App.js
@@ -1,22 +1,22 @@
-import React from "react";
-import "./index.css";
-import Navbar from "./components/Navbar";
+import React from "react"
+import "./index.css"
+import Navbar from "./components/Navbar"
 
 const App = () => {
-  return (
-    <div>
-      <section>
-        <Navbar></Navbar>
-      </section>
-      <section>Paralax</section>
-      <section>Services</section>
-      <section>Parallax</section>
-      <section>Portfolio1</section>
-      <section>Portfolio2</section>
-      <section>Portfolio3</section>
-      <section>Contact</section>
-    </div>
-  );
-};
+    return (
+        <div>
+            <section>
+                <Navbar></Navbar>
+            </section>
+            <section>Paralax</section>
+            <section>Services</section>
+            <section>Parallax</section>
+            <section>Portfolio1</section>
+            <section>Portfolio2</section>
+            <section>Portfolio3</section>
+            <section>Contact</section>
+        </div>
+    )
+}
 
-export default App;
+export default App
--- portfolio-web1/src/components/Navbar.jsx
+++ portfolio-web1/src/components/Navbar.jsx
@@ -1,26 +1,33 @@
-import "./navbar.css";
-import { FaFacebookF, FaInstagram, FaYoutube } from "react-icons/fa";
+import "./navbar.css"
+import { FaFacebookF, FaInstagram, FaYoutube } from "react-icons/fa"
+import { motion } from "framer-motion"
 
 const Navbar = () => {
-  return (
-    <div className="navbar">
-      {/*Sidebar*/}
-      <div className="wrapper">
-        <span>Jakub Dvořák</span>
-        <div className="social">
-          <a href="#">
-            <FaFacebookF />
-          </a>
-          <a href="#">
-            <FaInstagram />
-          </a>
-          <a href="#">
-            <FaYoutube />
-          </a>
+    return (
+        <div className="navbar">
+            {/*Sidebar*/}
+            <div className="wrapper">
+                <motion.span
+                    initial={{ opacity: 0, scale: 0.5 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 1 }}
+                >
+                    Jakub Dvořák
+                </motion.span>
+                <div className="social">
+                    <a href="#">
+                        <FaFacebookF />
+                    </a>
+                    <a href="#">
+                        <FaInstagram />
+                    </a>
+                    <a href="#">
+                        <FaYoutube />
+                    </a>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
 
-export default Navbar;
+export default Navbar
